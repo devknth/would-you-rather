1652e2ec638097946a9512eabf1451f13032d261
koreankenneth <koreankenneth@gmail.com>
2019-03-30 15:50:33 +0900

    toggle making

--- src/components/QList.js
+++ src/components/QList.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import QCard from './QCard'
 
+
+function toggleQList (e) {
+  console.log(e.target.innerHTML.toLowerCase())
+}
+
 class QList extends Component {
-  //click answer toggle
+
   
   render () {
     return (
       <div>
         <h3>Question List</h3>
+        <div className='nav'>
+          <ul>
+            <li activeClassName='active' onClick={e => toggleQList(e)}>Unvoted</li>
+            <li activeClassName='active' onClick={e => toggleQList(e)}>Voted</li>
+          </ul> 
+        </div>
         <ul>
           {this.props.questionIds.map((id) => (
             <li key={id}>
